bce22a0c80a44a0233019bab6ca11a5783656635
oskargiri <oskargiri87@gmail.com>
2024-04-15 13:07:28 +0545

    week1

--- Hello.java
+++ Hello.java
@@ -1,6 +1,87 @@
 public class Hello{
     public static void main(String[] args) {
-    //    * */ Write a program that prints Hello World! to the console. Save the program as Hello.java.
+         /*Write a program that prints Hello World! to the console. Save the program as Hello.java./* */
         System.out.println("Hello World:");
+        /*2. Extend/Modify the above program to print two additional lines:
+
+This is my first program
+
+I am on module ST4003CEM
+
+System.out.print (not println) and produces the same output. */
+
+/*public class Hello{
+    public static void main(String[]args){
+    System.out.print("This is my first program");
+    System.out.print("I am on module ST4003CEM");
+    }/* */
+    
+
+            System.out.print("This is my first program");
+            System.out.println("I am on module ST4003CEM");
+            /*. Write a program that prints the following output:
+
+A "quoted" String is
+
+'much' better if you learn
+
+the rules of "escape sequences."
+
+Also, "" represents an empty
+
+String. Don't forget: use \"
+
+instead of " !
+
+'' is not the same as " */
+
+System.out.println("A \"quoted\" String is");
+System.out.println("\'much\' better if you learn");
+System.out.println("the rules of \"escape sequence.\"");
+System.out.println("Also, \"\" represent an empty");
+System.out.println("String. Don\'t forget :use\"");
+System.out.println("instead of \" !");
+System.out.println("\'\' is not the same as\"");
+        /*WAP that prints the following pattern:
+
+        *
+        
+        **
+        
+        ***
+        
+        ****/ 
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+    /*5. WAP prints the following pattern:
+
+    *********
+    
+    *********
+    
+    *********
+    
+    *********/ 
+    System.out.println("**********");
+    System.out.println("**********");
+    System.out.println("**********");
+    System.out.println("**********");
+    /*
+
+ */
+System.out.println("A well-formed Java program has a main method with {and} braces.\n\n");
+
+
+System.out.println("A System.out.println statement has ( and ) and ");
+System.out.println("the starts and end with a \"character.");
+System.out.println("(But we type \"instead!)");
+System.out.println("What is the difference between");
+System.out.println("a\' and a\" ? Or between a\" and a\"?\n\n");
+System.out.println("One is what we see when we're typing our program. The other is what appears on");
+System.out.println("On the \"console\"");
+
     }
 }
